f06749aef956320a45d4a32188f09458e633e409
rarspace01 <rarspace01@gmail.com>
2013-01-06 14:18:20 +0000

    *ADD: -getter & setter -data handler for user part1

--- src/de/sfgmbh/datalayer/core/repositories/DataHandlerUser.java
+++ src/de/sfgmbh/datalayer/core/repositories/DataHandlerUser.java
@@ -1,16 +1,53 @@
 package de.sfgmbh.datalayer.core.repositories;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import de.sfgmbh.applayer.core.model.User;
 import de.sfgmbh.datalayer.core.definitions.IntfDataUser;
+import de.sfgmbh.datalayer.io.DataManagerPostgreSql;
 
 public class DataHandlerUser implements IntfDataUser{
 
 	@Override
 	public List<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> listUser = new ArrayList<User>();
+		User returnUser = null;
+
+		String SqlStatement = "SELECT * FROM public.user";
+
+		try {
+
+			ResultSet resultSet = DataManagerPostgreSql.getInstance().select(
+					SqlStatement);
+
+			while (resultSet.next()) {
+
+				returnUser = new User();
+
+				returnUser.setUserId_(resultSet.getInt("userid"));
+				returnUser.setLogin_(resultSet.getString("login"));
+				returnUser.setPass_(resultSet.getString("pass"));
+				returnUser.setSalt_(resultSet.getString("salt"));
+				returnUser.setMail_(resultSet.getString("mail"));
+				returnUser.setClass_(resultSet.getString("class"));
+				returnUser.setfName_(resultSet.getString("fname"));
+				returnUser.setlName_(resultSet.getString("lname"));
+				returnUser.setLastLogin_(resultSet.getLong("lastlogin"));
+
+				listUser.add(returnUser);
+				returnUser = null;
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return listUser;
 	}
 
 	@Override
