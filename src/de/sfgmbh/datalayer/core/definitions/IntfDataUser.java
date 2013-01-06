30785588e0bc0f05e670d033a028fe57f2931de9
rarspace01 <rarspace01@gmail.com>
2013-01-06 13:58:26 +0000

    *ADD: -user interface+ tbl class *FIX: -descr IntfDataRoom.java

---
+++ src/de/sfgmbh/datalayer/core/definitions/IntfDataUser.java
@@ -0,0 +1,59 @@
+package de.sfgmbh.datalayer.core.definitions;
+
+import java.util.List;
+
+import de.sfgmbh.applayer.core.model.User;
+
+/**
+ * Interface for handling the user table
+ * @author denis
+ *
+ */
+public interface IntfDataUser {
+
+	/**
+	 * 
+	 * @return List of User objects which are present in the database
+	 */
+	public List<User> getAll();
+
+	/**
+	 * 
+	 * @param iUserId
+	 *            - User id in the database
+	 * @return User object
+	 */
+	public User get(int UserId);
+
+	/**
+	 * 
+	 * @param searchQry - Search pattern
+	 * @return User objects
+	 */
+	public List<User> search(String searchQry);
+	
+	/**
+	 * 
+	 * @param toBeDeletedUser
+	 *            - the object which should be remove from the database
+	 * @return
+	 */
+	public void delete(User toBeDeletedUser);
+
+	/**
+	 * 
+	 * @param toBeSavedUser
+	 *            - object of type User to be saved in DB
+	 * @return - int for error handling
+	 */
+	public void save(User toBeSavedUser);
+	
+	/**
+	 * 
+	 * @param toBeUpdatedUser
+	 *            - object of type User to be udpated in DB
+	 * @return - int for error handling
+	 */
+	public void update(User toBeUpdatedUser);
+	
+}
---
+++ src/de/sfgmbh/datalayer/core/repositories/DataHandlerUser.java
@@ -0,0 +1,46 @@
+package de.sfgmbh.datalayer.core.repositories;
+
+import java.util.List;
+
+import de.sfgmbh.applayer.core.model.User;
+import de.sfgmbh.datalayer.core.definitions.IntfDataUser;
+
+public class DataHandlerUser implements IntfDataUser{
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User get(int UserId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> search(String searchQry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(User toBeDeletedUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(User toBeSavedUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User toBeUpdatedUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
