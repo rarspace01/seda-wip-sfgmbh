428a927d7fb74be3d9845801bf7e58c90034c3fb
denishamann@gmail.com <denishamann@gmail.com@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-07 10:42:02 +0000

    *UPDATE: -removed update, changed to save -added IntfDataChair

---
+++ trunk/src/de/sfgmbh/datalayer/core/definitions/IntfDataChair.java
@@ -0,0 +1,56 @@
+package de.sfgmbh.datalayer.core.definitions;
+
+import java.util.List;
+
+import de.sfgmbh.applayer.core.model.Chair;
+
+public interface IntfDataChair {
+
+	/**
+	 * 
+	 * @return List of Chair objects which are present in the database
+	 */
+	public List<Chair> getAll();
+
+	/**
+	 * 
+	 * @param iChairId
+	 *            - Chair id in the database
+	 * @return Chair object
+	 */
+	public Chair get(int ChairId);
+
+	/**
+	 * 
+	 * @param searchQry - Search pattern
+	 * @return Chair objects
+	 */
+	public List<Chair> search(String searchQry);
+
+	/**
+	 * 
+	 * @param filterQry - filter according to pattern.
+	 * <br/>Patter example: "{@code building=ERBA&level=2.1}"
+	 * <br/>"{@code building=FEKI&level=2&pc_min=10}"  
+	 *            
+	 * @return Chair objects
+	 */
+	public List<Chair> filter(String filterQry);
+	
+	/**
+	 * 
+	 * @param Chair
+	 *            - the object which should be remove from the database
+	 * @return
+	 */
+	public void delete(Chair Chair);
+
+	/**
+	 * 
+	 * @param Chair
+	 *            - object of type Chair to be saved in DB
+	 * @return - int for error handling
+	 */
+	public void save(Chair Chair);
+	
+}
