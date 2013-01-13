b58d7fbe90e49fb558b4b9c49e3bea3f0d3c8d02
hannes.sta@gmail.com <hannes.sta@gmail.com@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-13 21:32:28 +0000

    *UPDATE: Lecturer Start Tab Top Table

--- trunk/src/de/sfgmbh/comlayer/lecturer/model/StartTabTableTop.java
+++ trunk/src/de/sfgmbh/comlayer/lecturer/model/StartTabTableTop.java
@@ -1,17 +1,71 @@
 package de.sfgmbh.comlayer.lecturer.model;
 
-import javax.swing.table.*;
+import java.util.HashMap;
 
-public class StartTabTableTop extends DefaultTableModel {
+import javax.swing.table.DefaultTableModel;
+
+import de.sfgmbh.applayer.core.controller.ServiceManager;
+import de.sfgmbh.applayer.core.controller.SessionManager;
+import de.sfgmbh.applayer.core.definitions.IntfAppObserver;
+import de.sfgmbh.applayer.core.model.AppModel;
+import de.sfgmbh.applayer.core.model.Chair;
+import de.sfgmbh.applayer.core.model.Course;
+import de.sfgmbh.applayer.core.model.User;
+import de.sfgmbh.comlayer.core.controller.ViewHelper;
+
+public class StartTabTableTop extends DefaultTableModel implements IntfAppObserver {
 
 	private static final long serialVersionUID = 1L;
-	private Object[][] preFill = {
-			{"SEDA-WIP-B", "Benker", "WS1213", "4", "30", "2", "nein", "nein"},
-		};
-	private String[] preFillHeader = {"Bezeichnung", "Dozent", "Semester", "SWS", "Erw. Teilnehmer", "Termine", "Raumanfrage freigegeben", "\u00D6ffentlich"};
-	
+	private String[] header = {"Bezeichnung", "Art", "Dozent", "SWS", "Erw. Teilnehmer", "öffentlich", "Hidden"};
 	
 	public StartTabTableTop() {
-		this.setDataVector(preFill, preFillHeader);
+		AppModel.getInstance().repositoryCourse.register(this);
+		this.setColumnIdentifiers(header);
+		this.change("init");
+	}
+
+	public void change(String variant) {
+		ViewHelper vh = new ViewHelper();
+		HashMap<String, String> filter = new HashMap<String, String>();
+		User sessionUser = SessionManager.getInstance().getSession();
+		
+		if (sessionUser.getChair_() != null) {
+		
+			Chair sessionChair = sessionUser.getChair_();
+			
+			this.setRowCount(0);
+			
+			if (variant.equals("init")) {
+				filter.put("chair", sessionChair.getAcronym_());
+				filter.put("login", sessionUser.getLogin_());
+			} else {
+				filter.put("chair", sessionChair.getAcronym_());
+				filter.put("lecturer", ServiceManager.getInstance().getLecturerStartTab().getComboLecturer().getSelectedItem().toString());
+			}
+			
+			for (Course course : AppModel.getInstance().repositoryCourse.getByFilter(filter)){
+				try {
+					Object[] row = {
+							course.getCourseAcronym_(), 
+							course.getCourseKind_(),
+							course.getLecturer_().getlName_(), 
+							course.getSws_(), 
+							course.getExpectedAttendees_(), 
+							vh.getBoolean(course.isLecturerEnabled_()),
+							course
+							};
+					this.addRow(row);
+	
+				} catch (Exception e) {
+					AppModel.getInstance().appExcaptions.setNewException("Ein unbekannter Fehler ist aufgetreten! <br /><br />Fehler:<br />" + e.toString(), "Fehler!");
+				}
+				
+			}
+		}
+	}
+	
+	@Override
+	public void change() {
+		this.change("update");
 	}
 }
