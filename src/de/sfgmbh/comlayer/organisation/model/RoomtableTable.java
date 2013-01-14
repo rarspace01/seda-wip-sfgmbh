f37fcc2fb613689a503ba8a0d36c15c6269297d6
rarspace01 <rarspace01@gmail.com>
2013-01-14 21:01:33 +0000

    *FIX -UTF8

--- src/de/sfgmbh/comlayer/organisation/model/RoomtableTable.java
+++ src/de/sfgmbh/comlayer/organisation/model/RoomtableTable.java
@@ -8,10 +8,10 @@ public class RoomtableTable extends DefaultTableModel {
 	private Object[][] preFill = {
 			{"08:00-10:00 Uhr", "","", "SEDA-WIP-B in WP3/01.004", "", "SEDA-DMS-B in WP3/01.004"},
 			{"10:00-12:00 Uhr", "", "SEDA-WIP-B in WP3/01.004", "", "", "ISDL-ISS1-M in WP3/01.004"},
-			{"12:00-14:00 Uhr", "", "", "ISDL-ISS1-M �bung in WP3/01.004", "",""},
+			{"12:00-14:00 Uhr", "", "", "ISDL-ISS1-Mübung in WP3/01.004", "",""},
 			{"14:00-16:00 Uhr", "SEDA-EbIS-1 in WP3/04.004", "","", "", ""},
 			{"16:00-18:00 Uhr", "","", "", "", ""},
-			{"18:00-20:00 Uhr", "", "", "", "SEDA-EbIS-1 �bung in WP3/01.004", ""},
+			{"18:00-20:00 Uhr", "", "", "", "SEDA-EbIS-1 Übung in WP3/01.004", ""},
 			
 			{null, null, null, null, null, null, null, null, null},
 		};
