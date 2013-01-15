ec5af4be8c6989608f806731d410297b7baddd01
hannes.sta@gmail.com <hannes.sta@gmail.com@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-15 14:53:44 +0000

    *UPDATE: Combox Models JavaDoc und komplette Implementierung

--- trunk/src/de/sfgmbh/comlayer/core/model/CmbboxFilterBuildingid.java
+++ trunk/src/de/sfgmbh/comlayer/core/model/CmbboxFilterBuildingid.java
@@ -6,10 +6,19 @@ import de.sfgmbh.applayer.core.definitions.IntfAppObserver;
 import de.sfgmbh.applayer.core.model.AppModel;
 import de.sfgmbh.applayer.core.model.User;
 
+/**
+ * Model for allocation status combo boxes
+ * 
+ * @author hannes
+ *
+ */
 public class CmbboxFilterBuildingid extends DefaultComboBoxModel<String> implements IntfAppObserver {
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Create the model object
+	 */
 	public CmbboxFilterBuildingid() {
 		AppModel.getInstance().getRepositoryUser().register(this);
 		this.change();
@@ -17,9 +26,17 @@ public class CmbboxFilterBuildingid extends DefaultComboBoxModel<String> impleme
 
 	@Override
 	public void change() {
+
+		// Build and clean up the model on change - do not use
+		// removeAllElements() as it can cause null pointer exceptions when an
+		// observer model has null elements at any time
+		int initalSize = this.getSize();
 		this.addElement("<alle>");
 		for (User user : AppModel.getInstance().getRepositoryUser().getAllLecturer()){
 			this.addElement(user.getlName_());
+		}
+		for (int i = 0; initalSize > i; i++) {
+			this.removeElementAt(i+1);
 		}
 	}
 }
--- trunk/src/de/sfgmbh/comlayer/core/model/CmbboxFilterChairAcronym.java
+++ trunk/src/de/sfgmbh/comlayer/core/model/CmbboxFilterChairAcronym.java
@@ -6,17 +6,29 @@ import de.sfgmbh.applayer.core.definitions.IntfAppObserver;
 import de.sfgmbh.applayer.core.model.AppModel;
 import de.sfgmbh.applayer.core.model.Chair;
 
+/**
+ * Model for chair acronym combo boxes
+ * 
+ * @author hannes
+ *
+ */
 public class CmbboxFilterChairAcronym extends DefaultComboBoxModel<String> implements IntfAppObserver {
 
 	private static final long serialVersionUID = 1L;
 	private String variant_;
 	
+	/**
+	 * Create the model object
+	 */
 	public CmbboxFilterChairAcronym() {
 		this.variant_ = "normal";
 		AppModel.getInstance().getRepositoryChair().register(this);
 		this.change();
 	}
 	
+	/**
+	 * Create the model object based on a submitted variant string
+	 */
 	public CmbboxFilterChairAcronym(String variant) {
 		this.variant_ = variant;
 		AppModel.getInstance().getRepositoryChair().register(this);
@@ -25,6 +37,11 @@ public class CmbboxFilterChairAcronym extends DefaultComboBoxModel<String> imple
 
 	@Override
 	public void change() {
+		
+		// Build and clean up the model on change - do not use
+		// removeAllElements() as it can cause null pointer exceptions when an
+		// observer model has null elements at any time
+		int initalSize = this.getSize();
 		if (!this.variant_.equals("blank")) {
 			this.addElement("<alle>");
 		} else {
@@ -32,6 +49,9 @@ public class CmbboxFilterChairAcronym extends DefaultComboBoxModel<String> imple
 		}
 		for (Chair chair : AppModel.getInstance().getRepositoryChair().getAll()){
 			this.addElement(chair.getAcronym_());
+		}
+		for (int i = 0; initalSize > i; i++) {
+			this.removeElementAt(i+1);
 		}
 	}
 }
