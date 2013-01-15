6fed95159be1d1be7a4399fca63078b50ff6db6a
M.Serno@gmx.de <M.Serno@gmx.de@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-15 12:09:22 +0000

    Update BaseRdbtnTopLeft, BaseTab

--- trunk/src/de/sfgmbh/comlayer/core/controller/BaseRdbtnTopLeft.java
+++ trunk/src/de/sfgmbh/comlayer/core/controller/BaseRdbtnTopLeft.java
@@ -3,8 +3,6 @@ package de.sfgmbh.comlayer.core.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import net.miginfocom.swing.MigLayout;
-
 import de.sfgmbh.applayer.core.controller.CtrlBaseTab;
 import de.sfgmbh.comlayer.core.views.InfoDialog;
 
@@ -31,14 +29,15 @@ public class BaseRdbtnTopLeft implements ActionListener {
 			ViewManager.getInstance().getCoreBaseTab().getOrganisationTable().setVisible(false);
 			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getRoomTable());
 			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(true);
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().setVisible(false);
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().removeAll();
-			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().repaint();
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblRoomnumber(), "cell 0 0,aligny bottom");
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblBuildingid(), "cell 1 0,aligny bottom");
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblLevel(), "cell 2 0,aligny bottom");
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxRoomnumberFilter(), "cell 0 1,aligny bottom");
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxBuildingidFilter(), "cell 1 1,aligny bottom");
 			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxLevelFilter(), "cell 2 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().setVisible(true);
 		}
 		
 		//course-JRadioButton is pressed
@@ -47,8 +46,17 @@ public class BaseRdbtnTopLeft implements ActionListener {
 			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(false);
 			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getOrganisationTable());
 			ViewManager.getInstance().getCoreBaseTab().getOrganisationTable().setVisible(true);
-			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(false);
-			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getOrganisationTable());	
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().setVisible(false);
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().removeAll();
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblLehrveranstaltung(), "cell 0 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblLehrstuhl(), "cell 1 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblDozent(), "cell 2 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblSemester(), "cell 3 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxOrgaFilter(), "cell 0 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxChairFilter(), "cell 1 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxLecturerFilter(), "cell 2 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxSemesterFilter(), "cell 3 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().setVisible(true);
 		}
 	}
 }
