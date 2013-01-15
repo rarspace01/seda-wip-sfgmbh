a14515675bd2062d77ffae0999e9652064f70cd8
M.Serno@gmx.de <M.Serno@gmx.de@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-15 11:03:29 +0000

    Update BaseRdbtnTopLeft

--- trunk/src/de/sfgmbh/comlayer/core/controller/BaseRdbtnTopLeft.java
+++ trunk/src/de/sfgmbh/comlayer/core/controller/BaseRdbtnTopLeft.java
@@ -3,6 +3,8 @@ package de.sfgmbh.comlayer.core.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import net.miginfocom.swing.MigLayout;
+
 import de.sfgmbh.applayer.core.controller.CtrlBaseTab;
 import de.sfgmbh.comlayer.core.views.InfoDialog;
 
@@ -28,10 +30,22 @@ public class BaseRdbtnTopLeft implements ActionListener {
 			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(true);
 			ViewManager.getInstance().getCoreBaseTab().getOrganisationTable().setVisible(false);
 			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getRoomTable());
+			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(true);
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().removeAll();
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().repaint();
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblRoomnumber(), "cell 0 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblBuildingid(), "cell 1 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getLblLevel(), "cell 2 0,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxRoomnumberFilter(), "cell 0 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxBuildingidFilter(), "cell 1 1,aligny bottom");
+			ViewManager.getInstance().getCoreBaseTab().getMainFilterPanel().add(ViewManager.getInstance().getCoreBaseTab().getComboBoxLevelFilter(), "cell 2 1,aligny bottom");
 		}
 		
 		//course-JRadioButton is pressed
 		if (this.ctrlAction.equals("course")){
+			ViewManager.getInstance().getCoreBaseTab().getOrganisationTable().setVisible(true);
+			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(false);
+			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getOrganisationTable());
 			ViewManager.getInstance().getCoreBaseTab().getOrganisationTable().setVisible(true);
 			ViewManager.getInstance().getCoreBaseTab().getRoomTable().setVisible(false);
 			ViewManager.getInstance().getCoreBaseTab().getMainTableScrollPane().setViewportView(ViewManager.getInstance().getCoreBaseTab().getOrganisationTable());	
