914a7eb88ecd60e6b3e2551a3817f83eb120df88
annakupfer@yahoo.de <annakupfer@yahoo.de@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-17 00:34:48 +0000

    *ADD:CtrlChair.java

---
+++ trunk/src/de/sfgmbh/applayer/organisation/controller/CtrlChair.java
@@ -0,0 +1,48 @@
+package de.sfgmbh.applayer.organisation.controller;
+
+import de.sfgmbh.applayer.core.model.AppModel;
+import de.sfgmbh.applayer.core.model.Chair;
+
+/**
+ * Controller for chair management
+ * 
+ * @author anna
+ *
+ */
+public class CtrlChair {
+/*	
+	/**
+	 * Create or edit a chair if it is a valid chair
+	 * @param chair
+	 * @return true if the creation was successful
+	 
+	public boolean saveChair(Chair chair) {
+		
+		if (chair.validate()) {
+			if (chair.save()) {
+				return true;
+			}
+			return false;
+		} else {
+			return false;
+		}
+	}
+	*/
+
+	/**
+	 * Delete a chair if possible
+	 * @param chair
+	 * @return true on success
+	 */
+	public boolean delete(Chair chair) {
+		// Check if the users exists in the data base
+		Chair delChair = AppModel.getInstance().getRepositoryChair().get(chair.getChairId_());
+		
+		if (delChair != null) {
+			return AppModel.getInstance().getRepositoryChair().delete(delChair);
+		}
+		
+		return false;
+	}
+
+}
