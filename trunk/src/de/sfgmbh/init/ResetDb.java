f67bfc8d52bc8703f1173a193e10b8b9b6a33ba8
hannes.sta@gmail.com <hannes.sta@gmail.com@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-23 20:14:01 +0000

    *DELETE: Unused class

--- trunk/src/de/sfgmbh/init/ResetDb.java
+++ trunk/src/de/sfgmbh/init/ResetDb.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.BatchUpdateException;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
