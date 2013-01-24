6f96c8952c454f92362bcb30441aee6f1db741c5
denishamann@gmail.com <denishamann@gmail.com@7f88ce28-60ab-e2e2-24a5-c8b5582ad388>
2013-01-24 21:25:56 +0000

    *JAVADOC/COMMENTS/varenaming

--- trunk/src/de/sfgmbh/comlayer/timetable/controller/CoreTimetableTabBtnPdf.java
+++ trunk/src/de/sfgmbh/comlayer/timetable/controller/CoreTimetableTabBtnPdf.java
@@ -13,62 +13,79 @@ import de.sfgmbh.comlayer.core.controller.ViewManager;
 import de.sfgmbh.comlayer.core.views.InfoDialog;
 
 /**
+ * Action listener class for the PDF output
  * 
  * @author denis
- *
+ * 
  */
 public class CoreTimetableTabBtnPdf implements ActionListener {
-	
+
 	private String navAction_;
 	protected InfoDialog infoWindow;
-	
-	
+
+	/**
+	 * constructor
+	 */
 	public CoreTimetableTabBtnPdf() {
 		this.navAction_ = "default";
 	}
+
+	/**
+	 * constructor
+	 * 
+	 * @param action
+	 *            - String whcih declares the action
+	 */
 	public CoreTimetableTabBtnPdf(String action) {
 		this.navAction_ = action;
 	}
-	
+
+	/**
+	 * actionPerformed on button klick
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		IntfCtrlPdf pdfController;
 		// Pdf Button is pressed
 		if (this.navAction_.equals("pdfCreate")) {
-			
+
 			String sFilename = "";
 			JFileChooser fc = new JFileChooser();
-			
-			//setting *.pdf filter for save dialog
+
+			// setting *.pdf filter for save dialog
 			FileFilters filter = new FileFilters();
 			filter.addExtension("pdf");
 			filter.setDescription("PDF - Portable Document Format");
 			fc.setFileFilter(filter);
-			
+
 			fc.setSelectedFile(new File(sFilename));
 			if (fc.showSaveDialog(fc) == JFileChooser.APPROVE_OPTION) {
-			// if the user hasn't typed .pdf, we'll do it for him
-				if (!fc.getSelectedFile().getPath().toLowerCase().endsWith(
-						".pdf"))
-					{
-						fc.setSelectedFile(new File(fc.getSelectedFile() + ".pdf"));
-					}
-				String semester = ViewManager.getInstance().getCoreBaseTab().getComboBoxSemesterFilter().getSelectedItem().toString();
-				
-				//setPDF
-				pdfController=new CtrlPdf(fc.getSelectedFile().getAbsolutePath());
-				//add the specific content
-				pdfController.addContent("Vorlesungsplan - Semester: "+semester,ViewManager.getInstance().getCoreTimetableTab().getScrollPane_());
-				//close document
+				// if the user hasn't typed .pdf, we'll do it for him
+				if (!fc.getSelectedFile().getPath().toLowerCase()
+						.endsWith(".pdf")) {
+					fc.setSelectedFile(new File(fc.getSelectedFile() + ".pdf"));
+				}
+				String semester = ViewManager.getInstance().getCoreBaseTab()
+						.getComboBoxSemesterFilter().getSelectedItem()
+						.toString();
+
+				// setPDF
+				pdfController = new CtrlPdf(fc.getSelectedFile()
+						.getAbsolutePath());
+				// add the specific content
+				pdfController.addContent("Vorlesungsplan - Semester: "
+						+ semester, ViewManager.getInstance()
+						.getCoreTimetableTab().getScrollPane_());
+				// close document
 				pdfController.close();
 			}
-			
+
 		}
 		if (this.navAction_.equals("reset")) {
 			ViewManager.getInstance().getCoreTimetableTab().resetPlan();
 		}
 	}
-	
+
 	// Manage InfoWindow instance
 	public InfoDialog getInfoWindow(String msg) {
 		this.infoWindow = new InfoDialog(msg);
--- trunk/src/de/sfgmbh/comlayer/timetable/model/CoreTimetableTabTable.java
+++ trunk/src/de/sfgmbh/comlayer/timetable/model/CoreTimetableTabTable.java
@@ -6,42 +6,44 @@ import de.sfgmbh.applayer.core.definitions.IntfAppObserver;
 import de.sfgmbh.comlayer.core.controller.ViewManager;
 
 /**
+ * TableModel for the
  * 
  * @author denis
- *
+ * 
  */
-public class CoreTimetableTabTable extends DefaultTableModel implements IntfAppObserver {
+public class CoreTimetableTabTable extends DefaultTableModel implements
+		IntfAppObserver {
 
 	private static final long serialVersionUID = 1L;
-	private Object[][] preFill = {
-			
-			
-		};
-	private String[] preFillHeader = {"Uhrzeit", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"};
-	
-	
+	private Object[][] preFill_ = {
+
+	};
+	private String[] preFillHeader = { "Uhrzeit", "Montag", "Dienstag",
+			"Mittwoch", "Donnerstag", "Freitag" };
+
 	public CoreTimetableTabTable() {
-		this.setDataVector(preFill, preFillHeader);
+		// setting the table header
+		this.setDataVector(preFill_, preFillHeader);
 	}
-	
+
 	/**
 	 * disables edits on the table cells
+	 * 
 	 * @author denis
 	 */
 	@Override
-    public boolean isCellEditable(int row, int column) {
-        return false;
-    }
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
 
 	@Override
 	public void change() {
-		
-		//remove all rows
+
+		// remove all rows
 		this.setRowCount(0);
-		
-		//get all rooms from db
+
+		// get all rooms from db
 		ViewManager.getInstance().getCoreTimetableTab().reloadRoomTable();
-		
+
 	}
 }
-
